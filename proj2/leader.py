7a189362d1025f13f4df547f7ba1caa2fa9440dd
Valentine233 <494543506@qq.com>
2018-11-02 15:43:58 +0800

    final leader

--- proj2/leader.py
+++ proj2/leader.py
@@ -9,28 +9,17 @@ class Leader():
 	def __init__(self, follower_addr):
 		self.follower_addr = follower_addr
 		self.self_addr = ('', 12341)
-		self.broad_dest = ('<broadcast>', 7788) # broad socket fd
+		# self.broad_dest = ('<broadcast>', 7788) # broad socket fd
 		self.lock_map = {"k1":None, "k2":None, "k3":None}
 		self.map_ver = 0
 
 	def listenFollower(self):
-		# self.sk = [] # all sockets where each corresponds to a follower leader
-		# for i in range(len(follower_addr)):
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		# s.bind(*follower_addr[i])
 		self.s.bind(self.self_addr)
-		# s.listen(1) # backlog(here 1) is the maximum number of queued connections
-		# self.sk.append(s)
-		# self.broad_sk = socket.socket(socket.AF_INET, socket.SO_BROADCAST)
-		# self.sk.append(broad_sk)
 
 	def dealRequest(self):
 
 		while True:
-			# r_list, w_list, e_list = select(self.sk, [], [], 1)
-			# for s in r_list:
-				# conn, addr = s.accept()
-				# data = conn.recv(1024) # data is of type "action lock_name client_id" (e.g. "PREEMPT/RELEASE lock1 23", "UPDATEMAP 2")
 			data, address = self.s.recvfrom(1024)
 			data = data.decode('utf-8')
 			request = data.split(' ')
@@ -50,7 +39,6 @@ class Leader():
 					print("*** Request failure ***")
 					self.s.sendto(bytes(request[0] + " " + request[1] + " " + request[2] + " 0", 'utf-8'), address)
 					print("[send data]", request[0] + " " + request[1] + " " + request[2] + " 0")
-					# conn.sendall(bytes(request[0] + " " + request[1] + " " request[2] + " 0"))
 				elif flag == 1:
 					print("*** Request success ***")
 					self.updateMap(action, lock_name, client_id)
@@ -63,20 +51,15 @@ class Leader():
 					print("*** Inconsistent map ***")
 					self.s.sendto(bytes(request[0] + " " + str(self.map_ver) + " " + str(self.lock_map), 'utf-8'), address)
 					print("[send data]", request[0] + " " + str(self.map_ver) + " " + str(self.lock_map))
-					# conn.sendall(bytes(request[0] + " " + str(self.map_ver) + " " + str(self.lock_map)))
-
 				else:
 					# Your lock map is already up-to-date.
 					print("*** Consistent map ***")
 					self.s.sendto(bytes(request[0] + " " + str(self.map_ver) + " Yes", 'utf-8'), address)
 					print("[send data]", request[0] + " " + str(self.map_ver) + " Yes")
-					# conn.sendall(bytes(request[0] + " " + str(self.map_ver) + " Yes"))
 			else:
 				raise ValueError
 
 			time.sleep(5)
-
-			# conn.close()
 
 	def checkLock(self, action, lock_name, client_id):
 		if action == "PREEMPT":
@@ -99,7 +82,6 @@ class Leader():
 		print("[update map] lock_name:", lock_name, "; value:", self.lock_map[lock_name])
 
 	def sendUpdateCommand(self, request):
-		# self.broad_sk.sendto(bytes(request[0] + " " + request[1] + " " + request[2] + " 1"), self.broad_dest)
 		for addr in self.follower_addr:
 			self.s.sendto(bytes(request[0] + " " + request[1] + " " + request[2] + " 1", 'utf-8'), addr)
 		print("[broadcast data]", request[0] + " " + request[1] + " " + request[2] + " 1")

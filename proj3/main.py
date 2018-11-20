6328ec6610b076ec1d33ac173b36bf2b6f06492c
AliceZhang2016 <zmx1996@126.com>
2018-11-20 21:13:09 +0800

    Create main.py

---
+++ proj3/main.py
@@ -0,0 +1,34 @@
+import nameserver
+import dataserver
+
+numReplicate = 3
+
+def main():
+    global numReplicate
+    ns = NameServer(numReplicate)
+    ds1 = DataServer("node1")
+    ds2 = DataServer("node2")
+    ds3 = DataServer("node3")
+    ds4 = DataServer("node4")
+    
+    ns.add(ds1)
+    ns.add(ds2)
+    ns.add(ds3)
+    ns.add(ds4)
+
+    ds1.start()
+    ds2.start()
+    ds3.start()
+    ds4.start()
+
+    ds1.join()
+    ds2.join()
+    ds3.join()
+    ds4.join()
+
+    ns.operator()
+
+    return 0
+
+if __name__ == '__main__':
+    main()

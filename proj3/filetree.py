e0e8f3a7d79ef3ab003ecb793418bd7f7390e3df
Gudonko <33519763+Gudonko@users.noreply.github.com>
2018-11-21 00:43:36 +0800

    Update filetree.py

--- proj3/filetree.py
+++ proj3/filetree.py
@@ -14,13 +14,15 @@ class FileTree(object):
         path_folder = path.split('/')
         if isFound == 2:
             print("Wrong path!")
-            return
+            return 1
         elif isFound == 1:
             print("File already exists")
+            return 1
         else:
             new = TreeNode(folder, 1)
             new.parent = cur
             cur.child[folder] = new
+            return 0
 
 
     def find_node(self, path):
@@ -40,11 +42,15 @@ class FileTree(object):
 
     def listt(self, node, meta):
         chunkSize = 2 * 1024 * 1024
-        if node == self.root:
+        if node.name == self.root.name:
             node.name = '/'
-        print(node.name+'\t'+meta[node.name][0]+'\t'+int(meta[node.name][1]/chunkSize))
+        print(node.name+'\t'+str(meta[node.name][0])+'\t'+str(int(meta[node.name][1]/chunkSize)))
+        if node.name == self.root.name:
+            node.name = ''
         for key in list(node.child.keys()):
-            listt(node.child[key], meta)
+            if key == []:
+                return
+            self.listt(node.child[key], meta)
 
     def list_(self, meta):
         self.listt(self.root, meta)

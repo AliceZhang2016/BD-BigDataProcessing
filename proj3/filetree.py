1cef816050a6203e1b00b15a6993ccb2485b1c3c
Gudonko <33519763+Gudonko@users.noreply.github.com>
2018-11-24 02:17:00 +0800

    Update filetree.py

--- proj3/filetree.py
+++ proj3/filetree.py
@@ -8,10 +8,10 @@ class TreeNode(object):
 class FileTree(object):
     def __init__(self):
         self.root = TreeNode('', 0)
+        self.cur_node = self.root
 
     def insert_node(self, path, isFile):
         isFound, cur, folder = self.find_node(path)
-        path_folder = path.split('/')
         if isFound == 2:
             print("Wrong path!")
             return 1
@@ -19,7 +19,7 @@ class FileTree(object):
             print("File already exists")
             return 1
         else:
-            new = TreeNode(folder, 1)
+            new = TreeNode(folder, isFile)
             new.parent = cur
             cur.child[folder] = new
             return 0
@@ -27,10 +27,16 @@ class FileTree(object):
 
     def find_node(self, path):
         path_folder = path.split('/')
-        if path_folder[0] != '':
-            return 2, None, None
-        cur = self.root
-        for folder in path_folder[1::]:
+        cur = self.cur_node
+        for folder in path_folder:
+            if folder == '..':
+                cur = cur.parent
+                continue
+            elif folder == '.':
+                continue
+            elif folder == '':
+                print('Wrong path!')
+                return 2, None, None
             parent = cur
             cur = cur.child.get(folder)
             if cur is None:
@@ -49,15 +55,44 @@ class FileTree(object):
             path_child = path_cur + '/' + key
             if meta.get(path_child) is not None:
                 print(path_child + '\t' + str(meta[path_child][0]) + '\t' + str(int(meta[path_child][1] / chunkSize)))
+            elif meta.get(path_child) is None and node.child[key].isFile == True:
+                print('No data', path_child, 'in meta!')
+                return
             self.listt(node.child[key], meta, path_child)
 
     def list_(self, meta):
         self.listt(self.root, meta, path='')
 
-filetree = FileTree()
-filetree.insert_node('/m.py', True)
-filetree.insert_node('/ganlu', False)
-filetree.insert_node('/ganlu/test.py', True)
-meta = {'/m.py':[10,4], '/ganlu/test.py':[1,2]}
-filetree.list_(meta)
+    def ls_(self):
+        for key in list(self.cur_node.child.keys()):
+            if key == []:
+                return
+            print(key, end='\t')
+        print()
 
+    def cd_(self, folder_path):
+        path_split = folder_path.split('/')
+        for folder in path_split:
+            if folder == '..':
+                if self.cur_node.parent == None:
+                    return 'Wrong path!'
+                self.cur_node = self.cur_node.parent
+            elif folder == '.':
+                self.cur_node = self.cur_node
+            else:
+                if self.cur_node.child.get(folder) is None:
+                    return 'Wrong path!'
+                self.cur_node = self.cur_node.child[folder]
+
+    def get_whole_path_(self, filename):
+        list = []
+        temp = self.cur_node
+        path = ''
+        while temp.name != '':
+            list.append(temp.name)
+            temp = temp.parent
+        list.reverse()
+        for folder in list:
+            path = path + '/'
+            path = path + folder
+        return path+'/'+filename
